f5f0c2b65d9377c2780c2c1b6b8ceaad84abdcdb
choobs96 <37956681+choobs96@users.noreply.github.com>
2019-05-08 18:34:28 +0800

    Added saving future projection

--- UpdatedFA.R
+++ UpdatedFA.R
@@ -3,8 +3,8 @@ library(shiny)
 ui <- fluidPage(
   titlePanel("Financial Calculator"),
   
-  sidebarLayout(
-    sidebarPanel(
+  #sidebarLayout(
+    #sidebarPanel(
       helpText("Help you calculate your expenses"),
       
       selectInput("var", 
@@ -15,37 +15,56 @@ ui <- fluidPage(
                               "Loan"),
                   selected = "Food"),
       numericInput("numInput",
-                   "Enter your expenses in RM:",
-                   value = 1000,),
+                   "Enter your yearly saving at the end of the month
+                   in RM:",
+                   value = 1000),
+      numericInput("AIR",
+                   "Enter the current interest rate in %:",
+                   value = 5),
       sliderInput("range", 
                  label = "Years of Saving:",
-               min = 1, max = 50, value = 30)
-
+               min = 1, max = 50, value = 30),
+      hr(),
+      actionButton("action","Saving Projection"),
       
-    ),
+    #),
     
-    mainPanel(
-      textOutput("selected_var"),
-      textOutput("txtOutput"),
-      textOutput("years")
+    #mainPanel(
+    textOutput("selected_var"),
+    textOutput("txtOutput"),
+    textOutput("AIRout"),
+    textOutput("FV"),
+    plotOutput("plot")
       
-    )
-  )
+    #)
+  #)
+#)
 )
 
 server <- function(input, output) {
+  source("./function.R")
 
   output$selected_var <- renderText({ 
     paste("You have selected", input$var)
   })
   output$txtOutput <- renderText({
-    paste("You spent RM: ", input$numInput)
+    paste("Your yearly saving is RM: ", input$numInput)
+  })
+  output$FV <- renderText({
+  paste("Your future value for",input$range,"years is RM: ", as.numeric(future_val(input$numinput,input$AIR,input$range)))
+  })
+  output$AIRout <- renderText({
+    paste("Your annual rate of return is: ", input$AIR,"%")
+  })
+  v <- reactiveValues(data = NULL)
+  observeEvent(input$action, {
+    v$data <- seq(0,input$range,by=2)
   })
   
-  output$years <- renderText({ 
-  paste("You have chosen ",input$range[1],"years of saving")
+  output$plot <- renderPlot({
+    if (is.null(v$data)) return()
+    hist(v$data)
   })
-
   
 }
 
